1d292011f060043c61332ece5bddff6b08773836
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-12 15:57:53 -0500

    exercise 1 - 6 completed

---
+++ js/js_101.js
@@ -0,0 +1,27 @@
+// JS exercises to maintain and gain knowledge in writing JavaScript
+
+//  Exercise 1
+// On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
+let onMArsRightNow = false;
+
+// Exercise 2
+// Create a variable named fruits and assign it an array of strings containing the following fruits.
+// mango, banana, guava, kiwi, and strawberry.
+let fruits = ['mango', 'banana', 'guava', 'kiwi', 'strawberry'];
+
+//  Exercise 3
+//  Create a variable named vegetables and assign it an array of fruits containing the following vegetable names as strings:
+//  eggplant, broccoli, carrot, cauliflower, and zucchini
+let vegetables = ['eggplant', 'broccoli', 'carrot', 'cauliflower', 'zucchini'];
+
+// Exercise 4
+// Create a variable named numbers and assign it an array of numbers, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Exercise 5
+// Add the string "tomato" to the end of the fruits array.
+fruits.push('tomato');
+
+// Exercise 6
+// add the string "tomato" onto the end of the vegetables array.
+vegetables.push('tomato');
