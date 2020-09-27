028c0769e1ffc146c344e3668f000cd3882abf46
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-27 10:40:44 -0500

    exercise 43

--- js/js_101.js
+++ js/js_101.js
@@ -288,3 +288,13 @@ let circumference = function (x) {
     x = (x * 2) * Math.PI;
     return x;
 }
+
+// Exercise 43
+// Write a function definition named isVowel that takes in value and returns true if the value is a, e, i, o, u in upper or lower case.
+let isVowel = function (x) {
+    if (x === 'a' || x === 'e' || x === 'i' || x === 'o' || x === 'u' || x === 'A' || x === 'E' || x === 'I'|| x === 'O' || x === 'U') {
+        return true;
+    } else {
+        return false;
+    }
+}
